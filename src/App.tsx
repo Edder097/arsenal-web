407c5e619de32bdd89ff1ada69fcaa8e0258aaef
Edder097 <edderalves09@gmail.com>
2026-06-08 15:57:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ClienteLayout from './pages/ClienteLayout';
-import FilmmakerPainel from './pages/FilmakerPainel'; // 👈 Importando o painel novo
+import FilmmakerPainel from './pages/FilmakerPainel'; 
+import RoteiristaPainel from './pages/RoteiristaPainel'; // 👈 1. Importe o painel novo aqui
 
 export default function App() {
   const [caminhoAtual, setCaminhoAtual] = useState(window.location.pathname);
@@ -18,6 +19,11 @@ export default function App() {
   // 🎛️ Roteamento condicional nativo e limpo
   if (caminhoAtual === '/filmmaker') {
     return <FilmmakerPainel />;
+  }
+
+  // 🚀 2. ADICIONE A CONDICIONAL PARA A NOVA TELA DO ROTEIRISTA:
+  if (caminhoAtual === '/roteirista') {
+    return <RoteiristaPainel />;
   }
 
   // Qualquer outra rota padrão renderiza a tela de agendamento do cliente
